13a7c5847233fc1014d58a1359cce35931040fa2
Alex Dev <alex_dev_2020@mail.ru>
2020-01-23 21:44:45 +1000

    right_triangle.rb after code review

--- lesson_1/right_triangle.rb
+++ lesson_1/right_triangle.rb
@@ -5,21 +5,25 @@
 # 7, 24, 25
 # https://scientificmagazine.ru/images/PDF/2016/12/tablitsa-pifagorovykh-troek-chisel.pdf
 
+sides =[]
 # запрашиваем у юзера три стороны
 puts "Please input side a"
-a = gets.to_f
+sides << gets.to_f
 puts"Please input side b"
-b = gets.to_f
+sides << gets.to_f
 puts "Please input side c"
-c = gets.to_f
-# запилил условие для равнобедренного через регулярки 
-if a == b && b == c
-    puts"You have an isosceles triangle!"
+sides << gets.to_f
+# находим самую длинную сторону
+a, b, h =  sides.sort
+if a == b && b == h
+  puts"You have an isosceles triangle!"
 else
-    if a*a == b*b+c*c|| b*b == a*a + c*c || c*c == a*a + b*b
-           puts "Triangle is right"
-      else
-           puts "Triangle is not right"
-      end
+  if h**2 == a**2 + b**2
+    puts "Triangle is right"
+  else
+    puts "Triangle is not right"
+  end
 end
+
+
 
