8605cb95f24d5668ed739c18dd447228feecb735
Alex Dev <alex_dev_2020@mail.ru>
2020-02-04 00:09:41 +1000

    Created base structure of Train class

---
+++ lesson_3/train.rb
@@ -0,0 +1,55 @@
+class Train
+
+	attr_reader  :train_id, :train_type, :wagon_qty
+
+	def initialize(train_id, train_type, wagon_qty)
+# speed = 0 at creation
+		
+		@train_id = train_id
+		@type = type
+		@wagons = []
+		@speed = 0
+		@route = Route.new(first_station, last_station)
+    end
+
+    def speed_up(n)
+    	@speed += n
+    end
+
+    def speed_down(n)
+# more attractive cecking of speed > 0 in line using ?
+    	@speed = (@speed - n > 0 ) ? @speed - n : stop
+    end
+
+    def stop
+    	@speed = 0
+    end
+
+#перепилил оба метода add_wagon & detach_wagon, если поезд едет - выводится сообщение warning
+    def warning
+    	puts 'Train is running!!!'
+    end
+
+# очень интересаная реализация (подсмотрено у выпускников курса)
+# переменная @ wagons  представляет собой фактически заготовку массива строковых элементов вида [ 'wagon#1', 'wagon#2'..'wagon#' ]
+# который мы заполняем элементами при инициализации объекта класса
+# здесь в методе просто подсчитываем количество элементов этого массива, генерим очередной и добавляем(пушим)
+# здесь же проверяем условие speed == 0
+    def add_wagon
+    	(@speed == 0) ? @wagons << "wagon \##{wagons_qty + 1}" : warning 
+    end
+
+# detach wagon
+    def detach_wagon
+    	(@speed == 0) ? @wagons.pop : warning    	
+    end
+
+# количество вагонов 
+    def wagon_qty
+    	@wagons.length
+    end 
+
+    def route=(route)
+    	if route.is_a?(Route)
+    	
+    end
