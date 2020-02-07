f3374209832d30f1c50d3dea942da7640b7d0dc6
Alex Dev <alex_dev_2020@mail.ru>
2020-02-07 19:54:55 +1000

    Fixed after code review

--- lesson_3/station.rb
+++ lesson_3/station.rb
@@ -1,37 +1,33 @@
 class Station 
 
-# only name will getting from instance of the class
-  attr_reader  :st_name
 
-# def default st_name "main" & empty array of trains
-  def initialize(st_name = 'main')
-	@st_name = st_name
-	@trains = []
+
+
+  # only name will getting from instance of the class
+  attr_reader  :name, :trains
+
+  # def default st_name "main" & empty array of trains
+  def initialize(st_name)
+    @name = name
+    @trains = []
   end
 
-# arrival - the train added to array of trains
+  # arrival - the train added to array of trains
 
   def train_in(train)
-	@trains << train
-
+    @trains << train
   end
 
-# recall list of trains at the station (просто посмотреть список, наверное можно было запилить через attr_reader )
+ 
+  # get the list of trains by type - just use .select method of Array (нужно будет пилить подобный метод у классса Train)
 
-  def trains_list
-	@trains
+  def trains_by_type(train_type)
+    @trains.select{ |train| train.type == train_type }
   end
 
-# get the list of trains by type - just use .select method of Array (нужно будет пилить подобный метод у классса Train)
-
-def trains_by_type(train_type)
-	@trains.select{|train| train.type == train_type }
-end
-
-# departure just delete the train from array of trains (using .delete method of Array)
+  # departure just delete the train from array of trains (using .delete method of Array)
 
   def train_out(train)
-	@trains.delete(train)
+    @trains.delete(train)
   end
-
-
+end
