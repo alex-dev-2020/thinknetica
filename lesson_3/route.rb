6d654559dcf20325aea86e3c7ebc28ef0bac42cb
Alex Dev <alex_dev_2020@mail.ru>
2020-02-07 22:06:21 +1000

    Fixed after code review

--- lesson_3/route.rb
+++ lesson_3/route.rb
@@ -1,36 +1,30 @@
 class Route
 
-# в ТЗ  вывод только  списка станций (будут в массиве лежать)
+  # в ТЗ  вывод только  списка станций (будут в массиве лежать)
 
   attr_reader  :stations
 
-# при создании экземпляра класса указываются first & last 
-# объявляем массив с двумя элементами, остальные будем добавлять используя .insert 
+  # при создании экземпляра класса указываются first & last 
+  # объявляем массив с двумя элементами, остальные будем добавлять используя .insert 
 
-  def initialize(first_st, last_st)
-    @stations = [first_st, last_st]
+  def initialize(first_station, last_station)
+    @stations = [first_station, last_station]
   end
 
-#добавление  станции в маршрут (предполагается, что -1  - это индекс последнего элемента)
+  #добавление  станции в маршрут (предполагается, что -1  - это индекс последнего элемента)
 
-  def add_st(station)
+  def add_station(station)
     @stations.insert(station)(-2, station)
   end	
 
-# удаление станции из маршрута  метод .delete (нужно реализовать проверку != last_st && != first_st)
+  # удаление станции из маршрута  метод .delete (нужно реализовать проверку != last_st && != first_st)
 
-  def delete_st(station)
-	if station != station.first && station != station.last
+  def delete_station(station)
+    if station != station.first && station != station.last
 	  @stations.delete(station)
-	else
+    else
 	  puts 'Конечные точки маршрута удалить нельзя!'
     end
   end
-
-# вывод списка станций в маршруте ( .each без аргумента)
-
-  def show_list_st
-    @stations.each{ |station| puts station } 
-  end   
 end
 
